ab272c24b559173e9119da7a4f87e8185feced5c
Noor El-Din Walid <noorwalid72@yahoo.com>
2022-04-16 23:10:50 +0200

    Added editing to delete data from TableView.

--- VeryCreativesTask/Scenes/TabBar/Favorites/Views/FavoritesViewController.swift
+++ VeryCreativesTask/Scenes/TabBar/Favorites/Views/FavoritesViewController.swift
@@ -14,7 +14,9 @@ protocol FavoritesViewControllerProtocol: AnyObject, NavigationRoute {
 class FavoritesViewController: UIViewController, FavoritesViewControllerProtocol {
     @IBOutlet private weak var tableView: UITableView!
     private var favoritesPresenter: FavoritesPresenterProtocol?
-
+    private var isTableViewEditable = false
+    private var editButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         favoritesPresenter = FavoritesPresenter(favoritesView: self)
@@ -34,18 +36,25 @@ class FavoritesViewController: UIViewController, FavoritesViewControllerProtocol
     
     private func setupViews() {
         title = "Favorites"
-        let editButton = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editButtonTapped))
+        editButton = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editButtonTapped))
         editButton.tintColor = UIColor.systemYellow
         self.navigationItem.rightBarButtonItem  = editButton
     }
     
     @objc private func editButtonTapped() {
-        
+        if tableView.isEditing {
+            tableView.isEditing = false
+            editButton.title = "Edit"
+        } else {
+            tableView.isEditing = true
+            editButton.title = "Done"
+        }
     }
     
     private func setupTableView() {
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.setEditing(false, animated: true)
         tableView.register(UINib(nibName: Constants.XIBs.FavoriteMovieTableViewCell, bundle: nil), forCellReuseIdentifier: Constants.TableViewCells.FavoriteMovieCell)
     }
     
@@ -54,7 +63,7 @@ class FavoritesViewController: UIViewController, FavoritesViewControllerProtocol
             self?.tableView.reloadData()
         }
     }
-
+    
 }
 
 extension FavoritesViewController: UITableViewDelegate, UITableViewDataSource {
@@ -66,16 +75,30 @@ extension FavoritesViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: Constants.TableViewCells.FavoriteMovieCell, for: indexPath) as? FavoriteMovieTableViewCell else { return UITableViewCell() }
         guard let movie = favoritesPresenter?.favoritedMovies?[indexPath.row] else { return UITableViewCell() }
-                                                               
+        
         cell.configure(movie: movie)
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //Gives an error, can't unfavorite the movie
-//        favoritesPresenter?.navigateToMovie(at: indexPath.row)
+        //Gives an error + can't unfavorite the movie
+        //        favoritesPresenter?.navigateToMovie(at: indexPath.row)
     }
     
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        if (self.tableView.isEditing) {
+            return UITableViewCell.EditingStyle.delete
+        }
+        return UITableViewCell.EditingStyle.none
+    }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            //This line gives an error, but deletes data correctly from Core Data
+            favoritesPresenter?.deleteMovieFromFavorites(movie: (favoritesPresenter?.favoritedMovies?[indexPath.row])!)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            tableView.reloadData()
+           }
+    }
 }
