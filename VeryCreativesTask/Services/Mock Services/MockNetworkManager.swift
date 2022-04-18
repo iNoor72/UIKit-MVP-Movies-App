d692ce07e8e4a9febedb421b49f5157f35f2e8b2
Noor El-Din Walid <noorwalid72@yahoo.com>
2022-04-18 18:05:31 +0200

    Added tests for mocking network.

--- VeryCreativesTask/Services/Mock Services/MockNetworkManager.swift
+++ VeryCreativesTask/Services/Mock Services/MockNetworkManager.swift
@@ -11,16 +11,12 @@ import Alamofire
 class MockNetworkManager: NetworkService {
 
     func fetchMovies<T:Decodable>(page: Int, type: MovieType, completion: @escaping (T?, Error?) -> ()) {
-        AF.request("").responseDecodable { (response : DataResponse<T, AFError>) in
-            switch response.result {
-            case .failure(let error):
-                print("There was a problem fetching data form API. Error: \(error)")
-                completion(nil, error)
-                
-            case .success(let data):
-                print("Data was fetched successfully! Data: \(data)")
-                completion(data, nil)
-            }
+        if page == 1 {
+            let data = MovieResponse()
+            completion(data as? T, nil)
+        } else {
+            let error = NSError()
+            completion(nil, error)
         }
     }
 }
